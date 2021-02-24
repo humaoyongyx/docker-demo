6d15142cffb28dffd343bcfaa1390d8b56550ce6
issac.hu <humy6@shanghai-electric.com>
2021-02-24 10:08:26 +0800

    springboot

--- service/src/main/java/com/example/service/ServiceController.java
+++ service/src/main/java/com/example/service/ServiceController.java
@@ -4,6 +4,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletRequest;
+import java.util.Enumeration;
 import java.util.UUID;
 
 /**
@@ -18,6 +19,19 @@ public class ServiceController {
     @RequestMapping
     public String get(HttpServletRequest request) {
         System.out.println("get..." + uuid);
+        return request.getServerName() + ":" + request.getServerPort() + ":" + uuid;
+    }
+
+    @RequestMapping("/2")
+    public String get2(HttpServletRequest request, String name) {
+        System.out.println(name);
+        System.out.println("get2..." + uuid);
+        Enumeration<String> headerNames = request.getHeaderNames();
+        while (headerNames.hasMoreElements()) {
+            String header = headerNames.nextElement();
+            String value = request.getHeader(header);
+            System.out.println("header:" + header + ",value:" + value);
+        }
         return request.getServerName() + ":" + request.getServerPort() + ":" + uuid;
     }
 }
--- web/src/main/java/com/example/web/WebController.java
+++ web/src/main/java/com/example/web/WebController.java
@@ -4,7 +4,7 @@ import cn.hutool.http.HttpUtil;
 import com.example.web.http.HttpHelper;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,19 +19,10 @@ public class WebController {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(WebController.class);
 
-    @Autowired
-    ServiceClient serviceClient;
-
     @RequestMapping
     public String get(HttpServletRequest request) {
         LOGGER.info("get...");
         return request.getServerName() + ":" + request.getServerPort();
-    }
-
-    @RequestMapping("/2")
-    public String get2(HttpServletRequest request) {
-        LOGGER.info("get2...");
-        return serviceClient.get();
     }
 
     @RequestMapping("/3")
@@ -47,6 +38,13 @@ public class WebController {
     public String get4(HttpServletRequest request) {
         LOGGER.info("get4...");
         String result = HttpUtil.get("http://service/");
+        return result;
+    }
+
+    @RequestMapping("/r/{path}")
+    public String getR(HttpServletRequest request, @PathVariable(value = "path", required = false) String path) {
+        LOGGER.info("getR...");
+        String result = HttpUtil.get("http://service/" + path);
         return result;
     }
 
